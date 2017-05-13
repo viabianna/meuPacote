d59068bf0045425c6e3478d08b6d722a890f0f94
viabianna <bia.vianna@gmail.com>
2017-05-13 00:12:42 -0300

    ooo

---
+++ R/funcaosomar.R
@@ -0,0 +1,16 @@
+ #' Soma dois elementos
+ #'
+ #' soma dois elementos
+ #'
+ #' @param x primeiro elemento
+ #' @param y segundo elemento
+ #'
+ #' @examples
+ #' x <-1
+ #' y <-2
+ #' somar(x,y)
+ #'
+ #' @export
+
+somar <- function (x,y)
+  {x+y}
